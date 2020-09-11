96ca5f3f1b2455795b28cf1106d0835c6c81b31e
ssong10 <leesy1403@naver.com>
2020-09-11 18:17:41 +0900

    괄호변환

--- 이승열/괄호변환.py
+++ 이승열/괄호변환.py
@@ -1,17 +1,32 @@
-def solution(p):
-    answer = ''
+def wordsplit(w):
     tmp = 0
-    for i in p:
-        if i == '(':
-            if tmp < 0:
-                answer+= ')'
+    if w:
+        for i in range(len(w)):
+            if w[i] == '(':
+                tmp += 1
             else:
-                answer+= '('
+                tmp -= 1
+            if not tmp:
+                break
+        u, v = w[:i+1],w[i+1:]
+        print(u,v)
+        if correct(u):
+            return u + wordsplit(v)
+        else:
+            return '(' + wordsplit(v) + ')' + u[1:-1][::-1]
+    else:
+        return ''
+    
+def correct(w):
+    tmp = 0
+    for i in range(len(w)):
+        if tmp < 0:
+            return False
+        if w[i] == '(':
             tmp += 1
         else:
-            if tmp > 0:
-                answer += ')'
-            else:
-                answer += '('
             tmp -= 1
-    return answer
+    return True
+
+def solution(p):
+    return wordsplit(p)
